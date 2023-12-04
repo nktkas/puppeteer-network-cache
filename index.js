b1a9752758226d7da216d685c2a559af18d65457
NoName <noname@example.com>
2023-12-04 07:25:56 +0200

    Update

--- index.js
+++ index.js
@@ -2,23 +2,65 @@ import { PuppeteerExtraPlugin } from 'puppeteer-extra-plugin';
 import EventEmitter from 'eventemitter3';
 
 /**
- * @typedef {Object} HTTPResponse - Represents an HTTP response received by a page
- * @property {String} url - URL of the request
- * @property {String} method - HTTP request methods. GET | HEAD | POST | PUT | DELETE | CONNECT | OPTIONS | TRACE | PATCH
- * @property {Number} status - HTTP response status codes. Informational (100 – 199) | Successful (200 – 299) | Redirection (300 – 399) | Client error (400 – 499) | Server error (500 – 599)
- * @property {Object<string, string>[]} headers - An object with HTTP response headers
- * @property {Buffer} buffer - Buffer with the response body
- * @property {String} body - Text representation of the response body
- * @property {Number} date - Time to receive the response in milliseconds
+ * @typedef {Object} NetworkResourceTiming
+ * @property {Number} requestTime - Timing's requestTime is a baseline in seconds, while the other numbers are ticks in milliseconds relatively to this requestTime.
+ * @property {Number} proxyStart - Started resolving proxy.
+ * @property {Number} proxyEnd - Finished resolving proxy.
+ * @property {Number} dnsStart - Started DNS address resolve.
+ * @property {Number} dnsEnd - Finished DNS address resolve.
+ * @property {Number} connectStart - Started connecting to the remote host.
+ * @property {Number} connectEnd - Connected to the remote host.
+ * @property {Number} sslStart - Started SSL handshake.
+ * @property {Number} sslEnd - Finished SSL handshake.
+ * @property {Number} workerStart - Started running ServiceWorker. EXPERIMENTAL
+ * @property {Number} workerReady - Finished Starting ServiceWorker. EXPERIMENTAL
+ * @property {Number} workerFetchStart - Started fetch event. EXPERIMENTAL
+ * @property {Number} workerRespondWithSettled - Settled fetch event respondWith promise. EXPERIMENTAL
+ * @property {Number} sendStart - Started sending request.
+ * @property {Number} sendEnd - Finished sending request.
+ * @property {Number} pushStart - Time the server started pushing request. EXPERIMENTAL
+ * @property {Number} pushEnd - Time the server finished pushing request. EXPERIMENTAL
+ * @property {Number} receiveHeadersStart - Started receiving response headers. EXPERIMENTAL
+ * @property {Number} receiveHeadersEnd - Finished receiving response headers.
  */
 
 /**
- * @typedef {Object} HTTPRequest - Represents an HTTP request sent by a page
- * @property {String} url - URL of the request
- * @property {String} method - HTTP request methods. GET | HEAD | POST | PUT | DELETE | CONNECT | OPTIONS | TRACE | PATCH
- * @property {Object<string, string>[]} headers - An object with HTTP request headers
- * @property {String|undefined} postData - The request's post body, if any
- * @property {Number} date - Time to receive the request in milliseconds
+ * @typedef {Object} SecurityDetails
+ * @property {String} issuer - The name of the issuer of the certificate.
+ * @property {String} protocol - The security protocol being used.
+ * @property {String[]} subjectAlternativeNames - The list of subject alternative names (SANs) of the certificate.
+ * @property {String} subjectName - The name of the subject to which the certificate was issued.
+ * @property {Number} validFrom - Unix timestamp marking the start of the certificate's validity.
+ * @property {Number} validTo - Unix timestamp marking the end of the certificate's validity.
+ */
+
+/**
+ * @typedef {Object} HTTPRequest - Represents an HTTP request sent by a page.
+ * @property {String} url - The URL of the response.
+ * @property {String} method - The HTTP request method used. GET | HEAD | POST | PUT | DELETE | CONNECT | OPTIONS | TRACE | PATCH
+ * @property {Object<string, string>[]} headers - An object with HTTP headers associated with the request. All header names are lower-case.
+ * @property {String|undefined} postData - The request's post body, if any.
+ * @property {HTTPRequest[]} redirectChain - A redirectChain is a chain of requests initiated to fetch a resource.
+ * @property {String} resourceType - Contains the request's resource type as it was perceived by the rendering engine. Allowed Values: document, stylesheet, image, media, font, script, textTrack, xhr, fetch, prefetch, eventSource, webSocket, manifest, signedExchange, ping, CSPViolationReport, preflight, other
+ * @property {Number} date - Time to receive the request in milliseconds.
+ */
+
+/**
+ * @typedef {Object} HTTPResponse - Represents an HTTP response received by a page.
+ * @property {String} url - The URL of the response.
+ * @property {Object} remoteAddress - The IP address and port number used to connect to the remote server.
+ * @property {String} remoteAddress.ip
+ * @property {Number} remoteAddress.port
+ * @property {String} method - The HTTP request method used. GET | HEAD | POST | PUT | DELETE | CONNECT | OPTIONS | TRACE | PATCH
+ * @property {Number} status - The HTTP status code of the response. Informational (100 – 199) | Successful (200 – 299) | Redirection (300 – 399) | Client error (400 – 499) | Server error (500 – 599).
+ * @property {Object<string, string>[]} headers - An object with HTTP headers associated with the response. All header names are lower-case.
+ * @property {Buffer|undefined} buffer - Buffer with the response body.
+ * @property {String|undefined} body - Text representation of the response body.
+ * @property {Number} date - Time to receive the response in milliseconds.
+ * @property {Boolean} fromCache - True if the response was served from either the browser's disk cache or memory cache.
+ * @property {NetworkResourceTiming|null} timing - Timing information related to the response.
+ * @property {SecurityDetails} securityDetails - SecurityDetails if the response was received over the secure connection, or null otherwise.
+ * @property {HTTPRequest} request - A matching HTTPRequest object.
  */
 
 /**
@@ -42,60 +84,49 @@ export default class PuppeteerNetworkCache extends PuppeteerExtraPlugin {
     onBrowser(browser) {
         browser.networkCache = {
             /**
-             * Browser HTTPResponse array
-             */
-            response: [],
-
-            /**
              * Browser HTTPRequest array
              */
             request: [],
 
             /**
-             * Creates events: request (HTTPRequest) | response (HTTPResponse)
+             * Browser HTTPResponse array
+             */
+            response: [],
+
+            /**
+             * Creates events: "request" (HTTPRequest) | "response" (HTTPResponse)
              */
             event: new EventEmitter(),
         }
 
-        browser.networkCache.event.on('response', (response) => {
-            browser.networkCache.response.unshift(response);
-
-            browser.networkCache.response = browser.networkCache.response.slice(0, this.browserCacheLimit);
-        });
         browser.networkCache.event.on('request', (request) => {
             browser.networkCache.request.unshift(request);
 
             browser.networkCache.request = browser.networkCache.request.slice(0, this.browserCacheLimit);
+        });
+        browser.networkCache.event.on('response', (response) => {
+            browser.networkCache.response.unshift(response);
+
+            browser.networkCache.response = browser.networkCache.response.slice(0, this.browserCacheLimit);
         });
     }
 
     onPageCreated(page) {
         page.networkCache = {
             /**
-             * Browser HTTPResponse array
-             */
-            response: [],
-
-            /**
              * Browser HTTPRequest array
              */
             request: [],
 
             /**
-             * Creates events: request (HTTPRequest) | response (HTTPResponse)
+             * Browser HTTPResponse array
              */
-            event: new EventEmitter(),
+            response: [],
 
             /**
-             * Check the existence of a response and return it (if exists), using RegExp for validation
-             * @param {RegExp} urlRegex - RegExp template to search response by URL
-             * @param {Boolean} [globalCache=false] - Search response in the whole browser or only on the page
-             * @returns {HTTPResponse|null} Represents an HTTP response received by a page | null (Not found)
+             * Creates events: "request" (HTTPRequest) | "response" (HTTPResponse)
              */
-            existResponse: (urlRegex, globalCache = false) => {
-                const networkCache = globalCache ? page.browser().networkCache : page.networkCache;
-                return networkCache.response.find((response) => urlRegex.test(response.url)) ?? null;
-            },
+            event: new EventEmitter(),
 
             /**
              * Check the existence of a request and return it (if exists), using RegExp for validation
@@ -109,32 +140,14 @@ export default class PuppeteerNetworkCache extends PuppeteerExtraPlugin {
             },
 
             /**
-             * Wait for the response to appear, then return it
+             * Check the existence of a response and return it (if exists), using RegExp for validation
              * @param {RegExp} urlRegex - RegExp template to search response by URL
-             * @param {Number} [timeout=20000] - Waiting time for a response to be received
              * @param {Boolean} [globalCache=false] - Search response in the whole browser or only on the page
-             * @returns {HTTPResponse} Represents an HTTP response received by a page
-             * @throws Will throw an error, if the timeout expires
+             * @returns {HTTPResponse|null} Represents an HTTP response received by a page | null (Not found)
              */
-            waitResponse: async (urlRegex, timeout = 20000, globalCache = false) => {
-                return await new Promise((r, j) => {
-                    let response = page.networkCache.existResponse(urlRegex);
-                    if (response) return r(response);
-
-                    const networkCache = globalCache ? page.browser().networkCache : page.networkCache;
-                    const listener = function (response) {
-                        if (urlRegex.test(response.url)) {
-                            clearTimeout(timeoutTimer);
-                            networkCache.event.removeListener('response', listener);
-                            return r(response);
-                        }
-                    };
-                    let timeoutTimer = setTimeout(() => {
-                        networkCache.event.removeListener('response', listener);
-                        return j(new Error('Timeout wait response', { cause: { urlRegex, timeout, networkCache: page.networkCache } }));
-                    }, timeout);
-                    networkCache.event.on('response', listener);
-                });
+            existResponse: (urlRegex, globalCache = false) => {
+                const networkCache = globalCache ? page.browser().networkCache : page.networkCache;
+                return networkCache.response.find((response) => urlRegex.test(response.url)) ?? null;
             },
 
             /**
@@ -167,40 +180,40 @@ export default class PuppeteerNetworkCache extends PuppeteerExtraPlugin {
 
                 });
             },
+
+            /**
+             * Wait for the response to appear, then return it
+             * @param {RegExp} urlRegex - RegExp template to search response by URL
+             * @param {Number} [timeout=20000] - Waiting time for a response to be received
+             * @param {Boolean} [globalCache=false] - Search response in the whole browser or only on the page
+             * @returns {HTTPResponse} Represents an HTTP response received by a page
+             * @throws Will throw an error, if the timeout expires
+             */
+            waitResponse: async (urlRegex, timeout = 20000, globalCache = false) => {
+                return await new Promise((r, j) => {
+                    let response = page.networkCache.existResponse(urlRegex);
+                    if (response) return r(response);
+
+                    const networkCache = globalCache ? page.browser().networkCache : page.networkCache;
+                    const listener = function (response) {
+                        if (urlRegex.test(response.url)) {
+                            clearTimeout(timeoutTimer);
+                            networkCache.event.removeListener('response', listener);
+                            return r(response);
+                        }
+                    };
+                    let timeoutTimer = setTimeout(() => {
+                        networkCache.event.removeListener('response', listener);
+                        return j(new Error('Timeout wait response', { cause: { urlRegex, timeout, networkCache: page.networkCache } }));
+                    }, timeout);
+                    networkCache.event.on('response', listener);
+                });
+            },
         }
 
-        page.on('response', async (response) => {
-            let url = response.url();
-            let status = response.status();
-            let headers = response.headers();
-            let buffer;
-            let body;
-            if (status !== 204 && (status <= 299 || status >= 400)) {
-                buffer = await response.buffer();
-                if (response.request().resourceType() == 'image') {
-                    body = await buffer.toString('base64');
-                } else {
-                    body = await response.text();
-                }
-            }
-            let date = Date.now();
-
-            response = { url, status, headers, buffer, body, date };
-
-            page.networkCache.response.unshift(response);
-            page.networkCache.event.emit('response', response);
-            page.browser().networkCache.event.emit('response', response);
-
-            page.networkCache.response = page.networkCache.response.slice(0, this.pageCacheLimit);
-        });
         page.on('request', (request) => {
-            let url = request.url();
-            let method = request.method();
-            let headers = request.headers();
-            let postData = request.postData();
-            let date = Date.now();
-
-            request = { url, method, headers, postData, date };
+            let request = formatPptrHTTPRequest(request);
+            request.date = Date.now();
 
             page.networkCache.request.unshift(request);
             page.networkCache.event.emit('request', request);
@@ -208,5 +221,55 @@ export default class PuppeteerNetworkCache extends PuppeteerExtraPlugin {
 
             page.networkCache.request = page.networkCache.request.slice(0, this.pageCacheLimit);
         });
+        page.on('response', async (response) => {
+            let response = await formatPptrHTTPResponse(response);
+            response.date = Date.now();
+
+            page.networkCache.response.unshift(response);
+            page.networkCache.event.emit('response', response);
+            page.browser().networkCache.event.emit('response', response);
+
+            page.networkCache.response = page.networkCache.response.slice(0, this.pageCacheLimit);
+        });
     }
+}
+
+function formatPptrHTTPRequest(request) {
+    let url = request.url();
+    let method = request.method();
+    let headers = request.headers();
+    let postData = request.postData();
+    let redirectChain = request.redirectChain().map(req => formatPptrHTTPRequest(req));
+    let resourceType = request.resourceType();
+
+    return { url, method, headers, postData, date, redirectChain, resourceType };
+}
+async function formatPptrHTTPResponse(response) {
+    let request = formatPptrHTTPRequest(response.request());
+    let url = response.url();
+    let remoteAddress = response.remoteAddress();
+    let status = response.status();
+    let headers = response.headers();
+    let buffer;
+    let body;
+    if (status !== 204 && (status <= 299 || status >= 400)) {
+        buffer = await response.buffer();
+        if (request.resourceType == 'image') {
+            body = await buffer.toString('base64');
+        } else {
+            body = await response.text();
+        }
+    }
+    let fromCache = response.fromCache();
+    let timing = response.timing();
+    let securityDetails = {
+        issuer: response.securityDetails().issuer(),
+        protocol: response.securityDetails().protocol(),
+        subjectAlternativeNames: response.securityDetails().subjectAlternativeNames(),
+        subjectName: response.securityDetails().subjectName(),
+        validFrom: response.securityDetails().validFrom(),
+        validTo: response.securityDetails().validTo(),
+    }
+
+    return { url, remoteAddress, status, headers, buffer, body, fromCache, timing, securityDetails, request };
 }
